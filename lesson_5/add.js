d09a66ddbba9b2a85e5a9cf3d71745229774d2ca
David <dmitrienkodavid1401@gmail.com>
2025-05-26 02:48:55 +0600

    less0n5

---
+++ lesson_5/add.js
@@ -0,0 +1,15 @@
+let color = prompt("Цвет светофора")
+
+if (color === "Красный") {
+    console.log('Стой!!!')
+    alert('Стой')
+} else if (color === 'Зеленый') {
+    console.log ('Газуй')
+    alert('Газуй')
+} else if (color === 'Желтый') {
+    console.log('Жди')
+    alert('ЖДИ')
+} else  {
+    console.log("Неправильный ввод :)")
+    alert("Error неверный ввод")
+}
