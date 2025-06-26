95451f50219dfed8099970b8bae3e263e43ec2a2
David <dmitrienkodavid1401@gmail.com>
2025-06-26 17:47:04 +0600

    dz_6.3

--- DZ_6.3/app.js
+++ DZ_6.3/app.js
@@ -8,9 +8,10 @@ TextP.textContent = "";
 for ( const Tab of TabsBtn) {
     Tab.addEventListener("change",(event) =>{
         const id = event.target.id;
+        
             TextP.style.animation = "none";
             TextP.offsetHeight; 
-            TextP.style.animation = "fadeEffect 0.6s ease";
+            TextP.style.animation = "fadeEffect 0.8s ease";
 
 
             Image.style.animation = "none";
